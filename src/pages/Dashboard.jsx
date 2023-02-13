13d7ed4782f69d194f3c2ae0aea72799606cd448
Vivek <dev.vivekkhanal@gmail.com>
2023-02-13 23:16:58 +0530

    header started

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,11 +7,21 @@ const Dashboard = () => {
   const sidebarData = {};
   return (
     <div>
+      {/* sidebar */}
       <aside className="fixed w-[300px] border bg-gray-200 h-[100vh] ">
         <div className="p-5 flex">
           <img src={logo} alt="logo" />
         </div>
       </aside>
+
+{/* header */}
+<header>
+  <div></div>
+  <div>
+    <button className="px-3 py-2 bg-">+ Create survey</button>
+  </div>
+</header>
+
     </div>
   );
 };
